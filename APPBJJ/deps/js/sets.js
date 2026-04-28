4975976c4a88892895f64484107ebe2fb7c3b937
Пойдет <marmoknotofficial@gmail.com>
2026-04-29 02:26:41 +0300

    bjj site

---
+++ APPBJJ/deps/js/sets.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+        
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hide');
+hiddenElements.forEach((el) => observer.observe(el));
